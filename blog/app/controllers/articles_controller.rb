37ff7d020fc1fda0e226fba9043135946a9cb96e
Giselle Vanegas <vanegasgisellenyc@gmail.com>
2017-03-29 10:21:19 -0400

    completed 5, beginning 6 now

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -3,12 +3,33 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def show
     @article = Article.find(params[:id])
   end
 
   def index
     @articles = Article.all
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
   def create
